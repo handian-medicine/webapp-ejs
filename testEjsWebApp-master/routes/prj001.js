dfeb7c1cb7423ffde4d00ee1215361d4947a09d2
handian-medicine <handian_medicine@163.com>
2018-12-12 16:47:02 +0800

    数据展示页面修改，提交功能正常

--- testEjsWebApp-master/routes/prj001.js
+++ testEjsWebApp-master/routes/prj001.js
@@ -15,19 +15,20 @@ router.get('/', function (req, res, next) {
         //直接发起数据请求，获取所有prj001项目的案例
         var url = myconst.apiurl + "prj001/geninfo/";
         var authstring = req.cookies.prj001token.access_token;
+        // var page = req.body.page_num;
         console.log(">>> prj001.js access_token: " + authstring);
-
         var options = {
             url: url,
             headers: {
                 'Authorization': 'Bearer ' + authstring
             }
+            // page: page
         };
         //这里增加请求内容
         request(options, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 var archiveobjs = JSON.parse(body);
-                console.log(">>>prj001.js -> archiveobjs: ", archiveobjs);
+                console.log(">>>prj001.js -> archiveobjs: ", archiveobjs.results);
                 res.render('prj001', {
                     title: '流调项目-排卵障碍性异常子宫出血',
                     archives: archiveobjs.results,
@@ -55,6 +56,7 @@ router.get('/', function (req, res, next) {
             console.log(">>>Info used for prj001 authentication: " + JSON.stringify(loginData));
             request.post({url: url,form: loginData}, function (error, response, body) {
                 console.log(">>>Authentication results: " + body);
+                // console.log(">>>Page Num is:", page);
                 if (!error && response.statusCode == 200) {
                     var obj = JSON.parse(body); //由JSON字符串转换为JSON对象
                     // 成功后将token写入Cookie，maxAge为cookie过期时间
@@ -71,17 +73,18 @@ router.get('/', function (req, res, next) {
                     //进一步发起数据请求，获取所有prj001项目的案例
                     url = myconst.apiurl + "prj001/geninfo/";
                     var authstring = obj.access_token;
+                    // var page = req.body.page_num;
                     console.log(">>> prj001 access_token: " + authstring);
-
                     var options = {
                         url: url,
                         headers: {
                             'Authorization': 'Bearer ' + authstring
                         }
+                        // page: page
                     };
                     
                     request(options, function (error, response, body) {
-                        //console.log(">>>cookie里面没有prj001的access_token, prj001.js -> archiveobjs->id: ", archiveobjs.results[1].id);
+                        //console.log(">>>cookie里面没有prj001的access_token, prj001.js -> archiveobjs->id: ", archiveobjs[1].id);
                         if (!error && response.statusCode == 200) {
                             var archiveobjs = JSON.parse(body);
                             res.render('prj001', {
@@ -104,7 +107,109 @@ router.get('/', function (req, res, next) {
             res.redirect("login");
         }
     }
+});
+//分页请求
+router.post('/', function (req, res, next) {
+    console.log(">>>prj001.js router.post方法");
+    if (req.cookies.prj001token) {
+        //直接发起数据请求，获取所有prj001项目的案例
+        var url = myconst.apiurl + "prj001/geninfo/";
+        var authstring = req.cookies.prj001token.access_token;
+        var page = parseInt(req.body.page_num);
+        console.log(">>> prj001.js access_token: " + authstring);
+        console.log(">>>Page Num is:", page, typeof(page));
+        var options = {
+            url: url,
+            headers: {
+                'Authorization': 'Bearer ' + authstring
+            },
+            form: {"page":page}
+        };
+        //这里增加请求内容
+        request.post(options, function (error, response, body) {
+            console.log(">>>response", response);
+            if (!error && response.statusCode == 200) {
+                var archiveobjs = JSON.parse(body);
+                console.log(">>>prj001.js -> archiveobjs: ", archiveobjs);
+                
+                res.render('prj001', {
+                    title: '流调项目-排卵障碍性异常子宫出血',
+                    archives: archiveobjs.results,
+                    username: req.cookies.userinfo.email
+                }, function(err, html){console.log("test:", archiveobjs.results)});
+            } else {
+                console.log(">>>Getting archives met unknown error. " , error.error_description);
+                res.redirect("login");
+            }
+        });
+    }else {
+        if (req.cookies.userinfo) {
+            var url = myconst.apiurl + "o/token/";
+            var loginData = {
+                "username": req.cookies.userinfo.email,
+                "password": req.cookies.userinfo.password,
+                "grant_type": "password",
+                "scope": myconst.scope_prj001,
+                "client_id": myconst.client_id,
+                "client_secret": myconst.client_secret
+            };
+            console.log(">>>Info used for prj001 authentication: " + JSON.stringify(loginData));
+            request.post({url: url,form: loginData}, function (error, response, body) {
+                console.log(">>>Authentication results: " + body);
+                // console.log(">>>Page Num is:", page);
+                if (!error && response.statusCode == 200) {
+                    var obj = JSON.parse(body); //由JSON字符串转换为JSON对象
+                    // 成功后将token写入Cookie，maxAge为cookie过期时间
+                    res.cookie("prj001token", {
+                        "access_token": obj.access_token,
+                        "refresh_token": obj.refresh_token,
+                        "scope": obj.scope,
+                        "expires_in": obj.expires_in
+                    }, {
+                        maxAge: 1000 * 60 * 60 * 4,
+                        httpOnly: true
+                    }); //cookie 4小时有效时间
 
+                    //进一步发起数据请求，获取所有prj001项目的案例
+                    url = myconst.apiurl + "prj001/geninfo/";
+                    var authstring = obj.access_token;
+                    var page = req.body.page_num;
+                    if (typeof(parseInt(page)) != typeof(1)){
+                        page = 1;
+                    }
+                    console.log(">>> prj001 access_token: " + authstring);
+                    var options = {
+                        url: url,
+                        headers: {
+                            'Authorization': 'Bearer ' + authstring
+                        },
+                        form: {"page":page}
+                    };
+                    
+                    request(options, function (error, response, body) {
+                        //console.log(">>>cookie里面没有prj001的access_token, prj001.js -> archiveobjs->id: ", archiveobjs[1].id);
+                        if (!error && response.statusCode == 200) {
+                            var archiveobjs = JSON.parse(body);
+                            res.render('prj001', {
+                                title: '流调项目-排卵障碍性异常子宫出血',
+                                archives: archiveobjs.results,
+                                username: req.cookies.userinfo.email
+                            });
+                        } else {
+                            console.log(">>>Getting archives met unknown error. " + err.error_description);
+                            res.redirect("login");
+                        }
+                    });
+                } else {
+                    console.log(">>>Invoking access token met unknown error. " + err.error_description);
+                    res.redirect("login");
+                }
+            });
+        } else {
+            console.log(">>>Failed to find cookie with user info");
+            res.redirect("login");
+        }
+    }
 });
 //数据采集
 router.get('/datainput', function (req, res, next) {
