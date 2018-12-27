a1c847fa594ddc6e397d19098f2c6aba17ccfb98
handian-medicine <handian_medicine@163.com>
2018-12-27 16:35:50 +0800

    stable版本

--- testEjsWebApp-master/routes/prj001.js
+++ testEjsWebApp-master/routes/prj001.js
@@ -282,13 +282,14 @@ router.post('/datainputoptr', function (req, res, next) {
         console.log("prj001.js formdata:", req.body.formdata);
         console.log(">>>prj001.js -> options: ", options);
         request.post(options, function (error, response, body) {
-            console.log("response:", response.body);
+            console.log("response:", body);
             // console.log("response.statusCode: ", response.statusCode);
             if (!error && response.statusCode == 201) {
                 res.json({status:1, msg:"录入成功"});
                 // console.log("prj001.js ajax result:", res);
                 //res.render('datainput',{username: req.cookies.userinfo.email});
             } else {
+                res.json({status:0, msg:"请输入完整信息"});
                 console.log('response.statusCode wrong');
             }
         })
